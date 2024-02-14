61f0377320f50b47ef217c627fe45c361678f50b
samuel <berriens@gmail.com>
2024-02-14 21:39:29 +0100

    updated hyper-param

--- tab_pfn/__main__.py
+++ tab_pfn/__main__.py
@@ -24,9 +24,9 @@ def main() -> None:
     train_parser.add_argument("output_folder", type=str)
     train_parser.add_argument("--learning-rate", type=float, default=1e-4)
     train_parser.add_argument("--datasets", type=int, default=2**10)
-    train_parser.add_argument("--data", type=int, default=2**13)
+    train_parser.add_argument("--data", type=int, default=2**14)
     train_parser.add_argument("--data-ratio", type=float, default=0.6)
-    train_parser.add_argument("--batch-size", type=int, default=32)
+    train_parser.add_argument("--batch-size", type=int, default=256)
 
     args = parser.parse_args()
 
