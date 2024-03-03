e31ee27a8feda876284d5cdf4f9989b9ae5871ef
samuel <berriens@gmail.com>
2024-03-03 22:10:08 +0100

    lower learning rate

--- tab_pfn/__main__.py
+++ tab_pfn/__main__.py
@@ -22,7 +22,7 @@ def main() -> None:
     train_parser = sub_parser.add_parser("train")
     train_parser.add_argument("run_name", type=str)
     train_parser.add_argument("output_folder", type=str)
-    train_parser.add_argument("--learning-rate", type=float, default=1e-4)
+    train_parser.add_argument("--learning-rate", type=float, default=2.5e-5)
     train_parser.add_argument("--steps", type=int, default=400000)
     train_parser.add_argument("--batch-size", type=int, default=10)
     train_parser.add_argument("--data", type=int, default=2048)
